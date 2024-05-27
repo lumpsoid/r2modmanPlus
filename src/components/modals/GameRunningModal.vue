8de7b2d6e71e9aa0027bb14480eef2771e5e1aad
lumpsoid <79290581+lumpsoid@users.noreply.github.com>
2024-05-27 02:09:56 +0200

    fix: changed modal window when starting a game for more verbosity with runRaw changes

--- src/components/modals/GameRunningModal.vue
+++ src/components/modals/GameRunningModal.vue
@@ -3,10 +3,13 @@
         <div class="modal-background" @click="close"></div>
         <div class="modal-content">
             <div class='notification is-info'>
-                <h3 class="title" v-if="isSteamGame">{{ activeGame.displayName }} is launching via Steam</h3>
-                <h3 class="title" v-else>{{ activeGame.displayName }} is starting</h3>
+                <h3 class="title" v-if="isRawMode">{{ activeGame.displayName }} is launching via EXE file</h3>
+                <h3 class="title" v-else="isRawMode">{{ activeGame.displayName }} is starting using platform: {{ activeGame.activePlatform.storePlatform }}</h3>
                 <h5 class="title is-5">Close this message to continue modding.</h5>
-                <div v-if="isSteamGame">
+                <div v-if="isRawMode">
+                    <p>Path to the game exe file: {{ gameDirectory }}\{{ activeGame.exeName[0] }}</p>
+                </div>
+                <div v-else="isRawMode">
                     <p>If this is taking a while, it's likely due to Steam starting.</p>
                     <p>Please be patient, and have fun!</p>
                 </div>
@@ -20,14 +23,23 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 import Game from "../../model/game/Game";
-import { StorePlatform } from "../../model/game/StorePlatform";
+import ManagerSettings from "src/r2mm/manager/ManagerSettings";
 
 @Component
 export default class GameRunningModal extends Vue {
     @Prop({required: true})
     readonly activeGame!: Game;
+    get settings(): ManagerSettings {
+        return this.$store.getters['settings'];
+    }
 
-    isSteamGame = this.activeGame.activePlatform.storePlatform === StorePlatform.STEAM;
+    get isRawMode(): boolean {
+        return this.settings.getContext().gameSpecific.runRaw;
+    }
+
+    get gameDirectory(): string {
+        return this.settings.getContext().gameSpecific.gameDirectory || "undefined";
+    }
 
     close() {
         this.$store.commit('closeGameRunningModal');
